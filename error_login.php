e58eaf148cbad31109fd7ef651c3058197e6fcab
Ander Raso Vázquez <anderraso@gmail.com>
2018-03-05 23:15:01 +0100

    página de error de prueba añadida
    Signed-off-by: Ander Raso Vázquez <anderraso@gmail.com>

---
+++ error_login.php
@@ -0,0 +1,14 @@
+
+<?php
+// cabecera de la pagina
+include "includes/header.php";
+?>
+
+<!-- Una página de error de ejemplo -->
+  <h1>No se ha podido hacer login</h1>
+  <p>Ha habido un error al hacer login <a href="login_page.php">volver a intentar.</a></p>
+
+<?php
+// pie de pagina
+include "includes/footer.php";
+?>
