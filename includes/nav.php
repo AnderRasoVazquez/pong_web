2f7064bd579a9620f0779983ca3b4f2cfcf81c8e
Ander Raso Vázquez <anderraso@gmail.com>
2018-03-05 23:13:51 +0100

    página inicial añadida
    Signed-off-by: Ander Raso Vázquez <anderraso@gmail.com>

---
+++ includes/nav.php
@@ -0,0 +1,21 @@
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="index.php">PONG</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+        <!--
+        cuando estemos en la página del login añadimos la clase active
+        lo mismo para el de registrarse
+        <li class="nav-item active">-->
+          <a class="nav-link" href="login_page.php">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="registrarse.php">Registrarse</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+// cabecera de la pagina
+include "includes/header.php";
+?>
+
+  <div id="panel-principal" class="jumbotron capar-anchura mx-auto">
+   <h1 class="display-4">Pong... ¡¿Renovado?!</h1>
+   <p class="lead">Todos recordamos el clásico juego del Pong, pero te has preguntado alguna vez... ¿qué pasaría si le añadiésemos Power Ups?.</p>
+   <div class="text-center">
+     <img src="img/gameplay.gif" class="rounded img-fluid" alt="gameplay de pong">
+   </div>
+   <hr class="my-4">
+   <p>No esperes más para probarlo, regístrate para obtener una copia del juego.</p>
+   <p class="lead">
+     <a class="btn btn-primary btn-lg" href="login_page.php" role="button">¡Quiero jugar ya!</a>
+   </p>
+ </div>
+
+
+<?php
+// pie de pagina
+include "includes/footer.php";
+?>
