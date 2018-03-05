9a1f3d8ce96bbf9d0fd57cfd704d39e7ab6191f1
Ander Raso Vázquez <anderraso@gmail.com>
2018-03-05 23:14:49 +0100

    página de descargas añadida
    Signed-off-by: Ander Raso Vázquez <anderraso@gmail.com>

---
+++ descargar.php
@@ -0,0 +1,33 @@
+
+<?php
+// cabecera de la pagina
+include "includes/header.php";
+?>
+
+<h3>Has iniciado sesión, puedes descargar una copia del juego</h3>
+
+<div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Pong para Windows <span class="badge badge-danger">New!</span></h5>
+        <p class="card-text">Esta es la versión hecha para jugar en el sistema operativo Windows, descarga el archivo zip y descomprímelo para jugar.</p>
+        <a href="resources/Pong-windows.zip" class="btn btn-primary">Pong-windows.zip</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Pong para Linux <span class="badge badge-danger">New!</span></h5>
+        <p class="card-text">También tenemos una versión para Linux, descomprime el archivo tar.gz.</p>
+        <a href="resources/Pong-linux.tar.gz" class="btn btn-primary">Pong-linux.tar.gz</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php
+// pie de pagina
+include "includes/footer.php";
+?>
