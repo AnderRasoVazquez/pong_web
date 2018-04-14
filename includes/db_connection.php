8a8de9a9450ad7b42c05c3633ae9e46afc2d0715
David Perez Gomez <davidperezgo@gmail.com>
2018-04-14 19:01:14 +0200

    añadida clase para conectarse a la base de datos

---
+++ includes/db_connection.php
@@ -0,0 +1,60 @@
+<?php
+
+// clase para conectarse con MySQL
+class Connection {
+
+    private $user;
+    private $host;
+    private $password;
+    private $datbase;
+    private $connection;
+
+    public function __construct() {
+        $conf_file = "db_files/db.conf";
+        $json = json_decode(file_get_contents($conf_file));
+        $this->user = $json->{"user"};
+        $this->host = $json->{"host"};
+        $this->password = $json->{"password"};
+        $this->database = $json->{"database"};
+    }
+
+    public function connect() {
+        $this->connection = mysqli_connect(
+            $this->host,
+            $this->user,
+            $this->password,
+            $this->database
+        );
+        if (!$this->connection) {
+            die('No se pudo establecer la conexión con la base de datos: ' . mysqli_connect_error());
+        }
+    }
+
+    public function close() {
+        if ($this->connection) {
+            $this->connection->close();
+            $this->connection = null;
+        }
+    }
+
+    public function query($query) {
+        if ($this->connection) {
+            return $this->connection->query($query);
+        }
+    }
+
+    public function get_last_error() {
+        if ($this->connection) {
+            return $this->connection->error;
+        }
+    }
+
+    public function escape_string($string) {
+        if ($this->connection) {
+            return $this->connection->escape_string($string);
+        }
+    }
+
+}
+
+?>
